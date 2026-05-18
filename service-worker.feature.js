600e07e063d334e8e7043d1e093968a73493dbd5
Trong Phuoc <phuoctt@rabiloo.com>
2026-05-18 13:37:31 +0700

    Add service worker for caching and fetch handling

---
+++ service-worker.feature.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("novel-feature-app-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./feature.html",
+        "./manifest.feature.json",
+        "./novel.png",
+        "./novel.css",
+        "./pdf.css",
+        "./novel.js",
+        "./pdf.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
